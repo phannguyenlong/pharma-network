d41df29ad6f9d877a818ac3201f26161956551ba
longphannguyen <long.phannguyen@april9.com.au>
2025-09-10 19:11:14 +1000

    debug channel creation

--- scripts/envVar.sh
+++ scripts/envVar.sh
@@ -24,16 +24,19 @@ setGlobals() {
     export CORE_PEER_TLS_ROOTCERT_FILE=$PEER0_MANUFACTURER_CA
     export CORE_PEER_MSPCONFIGPATH=${PWD}/organizations/peerOrganizations/manufacturer.pharma.com/users/Admin@manufacturer.pharma.com/msp
     export CORE_PEER_ADDRESS=localhost:7051
+    export CORE_PEER_TLS_SERVERHOSTOVERRIDE=peer0.manufacturer.pharma.com
   elif [ $USING_ORG = "distributor" ]; then
     export CORE_PEER_LOCALMSPID="DistributorMSP"
     export CORE_PEER_TLS_ROOTCERT_FILE=$PEER0_DISTRIBUTOR_CA
     export CORE_PEER_MSPCONFIGPATH=${PWD}/organizations/peerOrganizations/distributor.pharma.com/users/Admin@distributor.pharma.com/msp
     export CORE_PEER_ADDRESS=localhost:9051
+    export CORE_PEER_TLS_SERVERHOSTOVERRIDE=peer0.distributor.pharma.com
   elif [ $USING_ORG = "retailer" ]; then
     export CORE_PEER_LOCALMSPID="RetailerMSP"
     export CORE_PEER_TLS_ROOTCERT_FILE=$PEER0_RETAILER_CA
     export CORE_PEER_MSPCONFIGPATH=${PWD}/organizations/peerOrganizations/retailer.pharma.com/users/Admin@retailer.pharma.com/msp
     export CORE_PEER_ADDRESS=localhost:11051
+    export CORE_PEER_TLS_SERVERHOSTOVERRIDE=peer0.retailer.pharma.com
   else
     echo "Unknown organization: $USING_ORG"
     exit 1
